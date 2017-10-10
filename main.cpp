9fd271f5b891ce810f87a180edb77ad2f331f577
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-10-10 05:45:07 -0700

    dumped visual studio, probably will be easier to properly link everything manually

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+#include <SimpleOpt.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+
+enum
+{
+	OPT_HELP,
+	OPT_LISTFILE,
+	OPT_SEARCH,
+	OPT_EXTRACT,
+	OPT_DEST,
+	OPT_FULLPATH,
+	OPT_LOWERCASE,
+	OPT_FLAG,
+	OPT_ARG
+};
+
+CSimpleOpt::SOption CMDLINE_OPTIONS[] = {
+	{ OPT_FLAG, "-a", SO_NONE },     // "-a"
+	{ OPT_FLAG, "-b", SO_NONE },     // "-b"
+	{ OPT_ARG,  "-f", SO_REQ_SEP },  // "-f ARG"
+	{ OPT_HELP, "-?", SO_NONE },     // "-?"
+	{ OPT_HELP, "--help", SO_NONE }, // "--help"
+	SO_END_OF_OPTIONS                // END
+};
 
 int main(int argc, char **argv) {
 
